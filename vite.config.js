d84be5cc2855f2075fb786c8a119f5d883394cd5
Hanzo Asashi <hansen@alenagm.com>
2023-09-24 05:34:29 +0800

    build(vite): support using https in local development
    add server hmr config to support using https and add cert in local
    development

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
+import fs from "fs";
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import {homedir} from "os";
+import {resolve} from "path";
+
+let host = "larasplade.local";
 
 export default defineConfig({
     plugins: [
@@ -21,4 +26,33 @@ export default defineConfig({
     ssr: {
         noExternal: ["vue", "@protonemedia/laravel-splade"]
     },
+    server: detectServerConfig(host),
 });
+
+function detectServerConfig(host) {
+    let keyPath = resolve(
+        homedir(),
+        "D:/Development/laragon/etc/ssl/laragon.key"
+    );
+    let certificatePath = resolve(
+        homedir(),
+        "D:/Development/laragon/etc/ssl/laragon.crt"
+    );
+
+    if (!fs.existsSync(keyPath)) {
+        return {};
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {};
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    };
+}
