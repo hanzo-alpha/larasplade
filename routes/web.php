1ad65bc41a54a2380709c50d17e8045e476ed2c2
Hanzo Asashi <hansen@alenagm.com>
2023-09-24 04:31:06 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['splade'])->group(function () {
+    // Registers routes to support the interactive components...
+    Route::spladeWithVueBridge();
+
+    // Registers routes to support password confirmation in Form and Link components...
+    Route::spladePasswordConfirmation();
+
+    // Registers routes to support Table Bulk Actions and Exports...
+    Route::spladeTable();
+
+    // Registers routes to support async File Uploads with Filepond...
+    Route::spladeUploads();
+
+    Route::get('/', static function () {
+        return view('welcome', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    });
+
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified',
+    ])->group(function () {
+        Route::view('/dashboard', 'dashboard')->name('dashboard');
+    });
+});
